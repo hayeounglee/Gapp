c0ffbc25e5f230b40bfcf360f5e96a9960f04e4d
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-01-31 10:21:19 +0900

    origin code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -42,7 +42,6 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       _wordsSpoken = "${result.recognizedWords}";
       _confidenceLevel = result.confidence;
-      _speechResultsList.add(result.recognizedWords); // Add result to the list
     });
   }
 
